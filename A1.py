9e729e4361b5407acd7bd0adf0deff6769af31c9
jrbartosch <jbartosch+jed@gmail.com>
2024-12-31 08:27:27 +0600

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,15 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print ('All the numbers have the Boolean value True.')
+else:
+    print ('At least one number has the Boolean value False.')
+
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+    print ('At least one number is greater than 0.')
+else:
+    print ('None of these numbers are greater than 0.')
